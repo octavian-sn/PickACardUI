b2478a5628908633e192f346fe56908da44bb652
Șulic Octavian <octavius1992@gmail.com>
2023-06-24 12:56:27 +0300

    Add project files.

---
+++ PickACardUI/CardPicker.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PickACardUI
+{
+    internal class CardPicker
+    {
+        static Random random = new Random();
+        public static string[] PickSomeCards(int numberOfCards)
+        {
+            string[] pickedCards = new string[numberOfCards];
+            for (int i = 0; i < numberOfCards; i++)
+            {
+                pickedCards[i] = RandomValue() + " of " + RandomSuit();
+            }
+            return pickedCards;
+        }
+
+        private static string RandomSuit()
+        {
+            int value = random.Next(1, 5);
+
+            switch (value)
+            {
+                case 1:
+                    return "Spades";
+                case 2:
+                    return "Hearts";
+                case 3:
+                    return "Clubs";
+                default:
+                    return "Diamonds";
+            }
+        }
+
+        private static string RandomValue()
+        {
+            int value = random.Next(1, 14);
+
+            switch (value)
+            {
+                case 1:
+                    return "Ace";
+                case 11:
+                    return "Jack";
+                case 12:
+                    return "Queen";
+                case 13:
+                    return "King";
+                default:
+                    return value.ToString();
+            }
+        }
+    }
+}
